638bcc2095ee77f1c035a7f93c407596420ff761
ziho9593 <no1hjyo@naver.com>
2021-04-27 15:36:47 +0900

    ViewController 개념 및 Xcode에서 확인

--- 02/MyAlbum/MyAlbum/ViewController.swift
+++ 02/MyAlbum/MyAlbum/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func Hello(_ sender: Any) {
+        print("Hello")
+    }
+    
+    
 }
 
