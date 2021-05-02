cfa97dd11acfe580c07aa6a50d2b24051f2f38ca
ziho9593 <no1hjyo@naver.com>
2021-05-02 13:20:51 +0900

    아웃렛 연결하기

--- 02/MyAlbum/MyAlbum/ViewController.swift
+++ 02/MyAlbum/MyAlbum/ViewController.swift
@@ -9,14 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceLabel: UILabel!
     var currentValue = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        priceLabel.text = "₩ \(currentValue)"
     }
     
     @IBAction func showAlret(_ sender: Any) {
-        let message = "가격은 ₩\(currentValue) 입니다."
+        let message = "가격은 \(currentValue) 입니다."
     
         let alret = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
@@ -25,6 +27,7 @@ class ViewController: UIViewController {
     
         let randomPrice = arc4random_uniform(10000) + 1
         currentValue = Int(randomPrice)
+        priceLabel.text = "₩ \(currentValue)"
     }
 
 }
