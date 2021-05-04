608fa4fdea6de872751dc8fd89fb55ccf1f9d085
ziho9593 <no1hjyo@naver.com>
2021-05-04 23:22:53 +0900

    Comment

--- 03/MyPlayground.playground/Contents.swift
+++ 03/MyPlayground.playground/Contents.swift
@@ -1,7 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//var greeting = "Hello, playground"
 
 
 let value = arc4random_uniform(100)
 print("--> \(value)")
+
+/*
+ 여기에 여러분이 하시고 싶은 말을
+ 길게 작성하셔서
+ 메시지를 남겨 놓으시면 되겠습니다.
+ 동료야 사랑해
+ */
