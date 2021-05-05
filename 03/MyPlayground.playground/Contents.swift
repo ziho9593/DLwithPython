7cb78ea727fcab8f34465fd8ef30a9821fe377f6
ziho9593 <no1hjyo@naver.com>
2021-05-05 11:01:28 +0900

    Scope

--- 03/MyPlayground.playground/Contents.swift
+++ 03/MyPlayground.playground/Contents.swift
@@ -88,3 +88,25 @@ let JasonOrMale = isJason || isMale
 
 let greatingMessage: String = isJason ? "Hello Jason" : "Hello Somebody"
 print("Msg: \(greatingMessage)")
+
+
+// --- Scope
+
+func hello() {
+    var hours = 50
+    let payPerHour = 10000
+    var salary = 0
+
+    if hours > 40 {
+        let extraHours = hours - 40
+        salary += extraHours * payPerHour * 2
+        hours -= extraHours
+    }
+
+    salary += hours * payPerHour
+
+    //print(extraHours)
+    print(hours)
+}
+
+hello()
