2adfd8e660efbb120007890c446eaab4f65ef5c6
ziho9593 <no1hjyo@naver.com>
2021-05-05 10:20:25 +0900

    Tuple

--- 03/MyPlayground.playground/Contents.swift
+++ 03/MyPlayground.playground/Contents.swift
@@ -12,3 +12,19 @@ print("--> \(value)")
  메시지를 남겨 놓으시면 되겠습니다.
  동료야 사랑해
  */
+
+// --- Tuple
+
+let coordinates = (4, 6)
+
+let x = coordinates.0
+let y = coordinates.1
+
+let coordinatesNamed = (x: 2, y: 3)
+
+let x2 = coordinatesNamed.x
+let y2 = coordinatesNamed.y
+
+let (x3, y3) = coordinatesNamed
+x3
+y3
