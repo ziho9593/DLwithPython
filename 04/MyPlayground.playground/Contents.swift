dc21fc8fec96884edd76ae69fc0f1a2af58e989d
ziho9593 <no1hjyo@naver.com>
2021-05-07 09:19:52 +0900

    Switch

--- 04/MyPlayground.playground/Contents.swift
+++ 04/MyPlayground.playground/Contents.swift
@@ -82,3 +82,62 @@ for i in halfClosedRange {
         print("gugu --> \(i) * \(j) = \(i * j)")
     }
 }
+
+
+// ---- Switch
+
+let num = 10
+switch num {
+case 0:
+    print("--> 0 입니다. ")
+case 0...10:
+    print("--> 0 10 사이 입니다. ")
+case 10:
+    print("--> 10 입니다. ")
+default:
+    print("--> 나머지 입니다. ")
+}
+
+let pet = "bird"
+switch pet {
+case "dog", "cat":
+    print("---> 집동물이네요? ")
+default:
+    print("---> 잘 모르겠습니다. ")
+}
+
+let num2 = 20
+switch num2 {
+case _ where num2 % 2 == 0:
+    print("---> 짝수")
+default:
+    print("---> 홀수")
+}
+
+//let coordinate = (x: 0, y: 10)
+//
+//switch coordinate {
+//case (0, 0):
+//    print("---> 원점 이네요")
+//case (_, 0):
+//    print("---> x축 이네요")
+//case (0, _):
+//    print("---> y축 이네요")
+//default:
+//    print("---> 좌표 어딘가")
+//}
+
+let coordinate = (x: 10, y: 10)
+
+switch coordinate {
+case (0, 0):
+    print("---> 원점 이네요")
+case (let x, 0):
+    print("---> x축 이네요, x:\(x)")
+case (0, let y):
+    print("---> y축 이네요, y:\(y)")
+case (let x, let y) where x == y:
+    print("---> x랑 y랑 같음, x, y = \(x), \(y)")
+case (let x, let y):
+    print("---> 좌표 어딘가, x, y = \(x), \(y)")
+}
