e1b3d9996e6b9ebe260f2d85b37e9265097d4631
ziho9593 <no1hjyo@naver.com>
2021-05-09 13:09:24 +0900

    Optional 도전과제

--- 05/Optional.playground/Contents.swift
+++ 05/Optional.playground/Contents.swift
@@ -49,3 +49,26 @@ printParsedInt2(from: "Hello")
 
 // Nil coalescing > 박스를 까봤더니, 값이 없으면 디폴트 값을 줘보자
 let myCarName: String = carName ?? "Model S"
+
+
+// ---- 도전 과제
+// 1. 최애 음식 이름을 담는 변수를 작성하시고 (String?),
+var favoriteFood: String? = "냉면"
+
+// 2. 옵셔널 바인딩을 이용해서 값을 확인해보기
+if let foodName = favoriteFood {
+    print(foodName)
+} else {
+    print("최애 음식 없음")
+}
+
+// 3. 닉네임을 받아서 출력하는 함수 만들기, 조건 입력 파라미터는 String?
+func printNickName(name: String?) {
+    guard let nickName = name else {
+        print("닉네임 없음")
+        return
+    }
+    print(nickName)
+}
+printNickName(name: "James")
+printNickName(name: nil)
