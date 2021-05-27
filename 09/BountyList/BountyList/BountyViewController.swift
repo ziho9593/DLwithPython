9b4993417af2a0eebb4b4aeae5651dffa646e009
ziho9593 <no1hjyo@naver.com>
2021-05-27 11:02:38 +0900

    Custom Cell

--- 09/BountyList/BountyList/BountyViewController.swift
+++ 09/BountyList/BountyList/BountyViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 800000000, 77000000, 120000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,22 +19,28 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     // UITableViewDataSource
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return bountyList.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.ImgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
         return cell
     }
     
-    
     // UITableViewDelegate
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("--> \(indexPath.row)")
     }
+}
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var ImgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
 }
