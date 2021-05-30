d6c0496d423564a152c5890aaf9de46d342d690c
ziho9593 <no1hjyo@naver.com>
2021-05-30 13:17:50 +0900

    Present ViewController

---
+++ 09/BountyList/BountyList/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  BountyList
+//
+//  Created by 한지호 on 2021/05/30.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
