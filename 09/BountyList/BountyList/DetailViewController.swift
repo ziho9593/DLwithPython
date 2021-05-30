544e51a73099c80fdc402e1dd92ac83a133e5697
ziho9593 <no1hjyo@naver.com>
2021-05-30 19:25:49 +0900

    ViewController 간 데이터 전달

--- 09/BountyList/BountyList/DetailViewController.swift
+++ 09/BountyList/BountyList/DetailViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var ImgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+    
+    var name: String?
+    var bounty: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    func updateUI() {
+        if let name = self.name, let bounty = self.bounty {
+            let img = UIImage(named: "\(name).jpg")
+            ImgView.image = img
+            nameLabel.text = name
+            bountyLabel.text = "\(bounty)"
+        }
     }
     
     @IBAction func close(_ sender: Any) {
