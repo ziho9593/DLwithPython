e8bd59d912a725caacabdebe7c032c1de5a8a787
ziho9593 <no1hjyo@naver.com>
2021-06-03 20:56:11 +0900

    MVVM으로 리팩터링하기

--- 10/Refactor/BountyList/BountyInfo.swift
+++ 10/Refactor/BountyList/BountyInfo.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
-class BountyInfo: NSObject {
-
+struct BountyInfo {
+    let name: String
+    let bounty: Int
+    
+    var image: UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    
+    init(name: String, bounty:Int) {
+        self.name = name
+        self.bounty = bounty
+    }
 }
