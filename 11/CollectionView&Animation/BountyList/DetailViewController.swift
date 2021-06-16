e168a86eac5128fd276c71b1601fe059894e3e77
ziho9593 <no1hjyo@naver.com>
2021-06-16 19:43:20 +0900

    Animating View Properties

--- 11/CollectionView&Animation/BountyList/DetailViewController.swift
+++ 11/CollectionView&Animation/BountyList/DetailViewController.swift
@@ -44,37 +44,55 @@ class DetailViewController: UIViewController {
     }
     
     private func prepareAnimation() {
-        nameLabelCenterX.constant = view.bounds.width
-        bountyLabelCenterX.constant = view.bounds.width
+//        nameLabelCenterX.constant = view.bounds.width
+//        bountyLabelCenterX.constant = view.bounds.width
+        
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: 0).scaledBy(x: 3, y: 3).rotated(by: 180)
+        
+        nameLabel.alpha = 0
+        bountyLabel.alpha = 0
     }
     
     private func showAnimation() {
-        nameLabelCenterX.constant = 0
-        bountyLabelCenterX.constant = 0
-        
-//        UIView.animate(withDuration: 0.3,
-//                       delay: 0.1,
-//                       options: .curveEaseIn,
-//                       animations: {
+//        nameLabelCenterX.constant = 0
+//        bountyLabelCenterX.constant = 0
+//
+////        UIView.animate(withDuration: 0.3,
+////                       delay: 0.1,
+////                       options: .curveEaseIn,
+////                       animations: {
+////            self.view.layoutIfNeeded()
+////        }, completion: nil)
+//
+//        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
 //            self.view.layoutIfNeeded()
 //        }, completion: nil)
+//
+//        UIView.transition(with: ImgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+//    }
         
-        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
-            self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 1, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
         }, completion: nil)
         
         UIView.transition(with: ImgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
-    }
+	}
     
     func updateUI() {
-        
         if let bountyInfo = viewModel.bountyInfo {
             ImgView.image = bountyInfo.image
             nameLabel.text = bountyInfo.name
             bountyLabel.text = "\(bountyInfo.bounty)"
         }
     }
-    
+
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
