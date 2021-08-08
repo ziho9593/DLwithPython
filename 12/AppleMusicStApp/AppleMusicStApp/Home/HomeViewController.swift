5016f811a270651df9f1fd2246d1bd77611525b5
ziho9593 <no1hjyo@naver.com>
2021-08-08 20:25:57 +0900

    Home 화면 CollectionView Header 구현 2 - Code 작성

--- 12/AppleMusicStApp/AppleMusicStApp/Home/HomeViewController.swift
+++ 12/AppleMusicStApp/AppleMusicStApp/Home/HomeViewController.swift
@@ -42,7 +42,21 @@ extension HomeViewController: UICollectionViewDataSource {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
             // TODO: 헤더 구성하기
-            return UICollectionReusableView()
+            guard let item = trackManager.todaysTrack else {
+                return UICollectionReusableView()
+            }
+            
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TrackCollectionHeaderView", for: indexPath) as? TrackCollectionHeaderView else {
+                return UICollectionReusableView()
+            }
+            
+            header.update(with: item)
+            header.tapHandler = { item -> Void in
+                // Player 를 띄운다
+                print("---> item title: \(item.convertToTrack()?.title)")
+            }
+            
+            return header
         default:
             return UICollectionReusableView()
         }
