fd77288ef89b140867a10e2b3ed1b06994da38c3
ziho9593 <no1hjyo@naver.com>
2021-08-17 17:45:20 +0900

    PlayerView 재생시간 실시간 업데이트 및 Seeking 구현

--- 12/AppleMusicStApp/AppleMusicStApp/Player/PlayerViewController.swift
+++ 12/AppleMusicStApp/AppleMusicStApp/Player/PlayerViewController.swift
@@ -33,6 +33,9 @@ class PlayerViewController: UIViewController {
         updatePlayButton()
         updateTime(time: CMTime.zero)
         // TODO: TimeObserver 구현
+        timeObserver = simplePlayer.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 10), queue: DispatchQueue.main, using: { time in
+            self.updateTime(time: time)
+        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,8 +46,9 @@ class PlayerViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        // TODO: 뷰나갈때 처리 > 심플플레이어
-        
+        // TODO: 뷰 나갈 때 처리 > 심플플레이어
+        simplePlayer.pause()
+        simplePlayer.replaceCurrentItem(with: nil)
     }
     
     @IBAction func beginDrag(_ sender: UISlider) {
@@ -57,6 +61,11 @@ class PlayerViewController: UIViewController {
     
     @IBAction func seek(_ sender: UISlider) {
         // TODO: 시킹 구현
+        guard let currentItem = simplePlayer.currentItem else { return }
+        let position = Double(sender.value) // 0 이상 1 이하의 값
+        let seconds = position * currentItem.duration.seconds
+        let time = CMTime(seconds: seconds, preferredTimescale: 100)
+        simplePlayer.seek(to: time)
     }
     
     @IBAction func togglePlayButton(_ sender: UIButton) {
@@ -89,13 +98,13 @@ extension PlayerViewController {
         // currentTime label, totalduration label, slider
         
         // TODO: 시간정보 업데이트, 심플플레이어 이용해서 수정
-        currentTimeLabel.text = secondsToString(sec: 0.0)   // 3.1234 >> 00:03
-        totalDurationLabel.text = secondsToString(sec: 0.0)  // 39.2045  >> 00:39
+        currentTimeLabel.text = secondsToString(sec: simplePlayer.currentTime)   // 3.1234 >> 00:03
+        totalDurationLabel.text = secondsToString(sec: simplePlayer.totalDurationTime)  // 39.2045  >> 00:39
         
         if isSeeking == false {
-            // 노래 들으면서 시킹하면, 자꾸 슬라이더가 업데이트 됨, 따라서 시킹아닐때마 슬라이더 업데이트하자
+            // 노래 들으면서 시킹하면, 자꾸 슬라이더가 업데이트 됨, 따라서 시킹이 아닐 때만 슬라이더 업데이트.
             // TODO: 슬라이더 정보 업데이트
-            
+            timeSlider.value = Float(simplePlayer.currentTime/simplePlayer.totalDurationTime)
         }
     }
     
