f326f8184655b160900b7d2953e37a9b63bc074b
ziho9593 <no1hjyo@naver.com>
2021-10-20 18:56:37 +0900

    Keyboard에 따라 InputView 위치 변경

--- 13/TodoList/TodoList/TodoListViewController.swift
+++ 13/TodoList/TodoList/TodoListViewController.swift
@@ -24,8 +24,9 @@ class TodoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // TODO: 키보드 디텍션
-        
+        // [O] TODO: 키보드 디텍션
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillHideNotification, object: nil)
         
         // [O] TODO: 데이터 불러오기
         todoListViewModel.loadTasks()
@@ -53,14 +54,26 @@ class TodoListViewController: UIViewController {
         // and tableview reload or update
     }
     
-    // TODO: BG 탭했을때, 키보드 내려오게 하기
+    // [O] TODO: BG 탭했을때, 키보드 내려오게 하기
+    @IBAction func tapBG(_ sender: Any) {
+        inputTextField.resignFirstResponder()
+    }
 }
 
 extension TodoListViewController {
     @objc private func adjustInputView(noti: Notification) {
         guard let userInfo = noti.userInfo else { return }
-        // TODO: 키보드 높이에 따른 인풋뷰 위치 변경
+        // [O] TODO: 키보드 높이에 따른 인풋뷰 위치 변경
+        guard let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
         
+        if noti.name == UIResponder.keyboardWillShowNotification {
+            let adjustmentHeigth = keyboardFrame.height - view.safeAreaInsets.bottom
+            inputViewBottom.constant = adjustmentHeigth
+        } else {
+            inputViewBottom.constant = 0
+        }
+        
+        print("---> Keyboard End Frame: \(keyboardFrame)")
     }
 }
 
