da37a44207f6d22368b5a85f9108551283a05e9e
ziho9593 <ziho9593@gmail.com>
2022-07-14 10:41:03 +0900

    도전: 이미지 뷰어 만들기

--- Books/Swift-Basic/03/ImageViewer/ImageViewer/ViewController.swift
+++ Books/Swift-Basic/03/ImageViewer/ImageViewer/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var numImage = 1
+    var maxImage = 6
+    
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imgView.image = UIImage(named: "1.png")
     }
 
-
+    @IBAction func btnBefore(_ sender: UIButton) {
+        numImage -= 1
+        if (numImage < 1) {
+            numImage = maxImage
+        }
+        
+        let imageName = String(numImage) + ".png"
+        imgView.image = UIImage(named: imageName)
+    }
+    
+    @IBAction func btnAfter(_ sender: UIButton) {
+        numImage += 1
+        if (numImage > maxImage) {
+            numImage = 1
+        }
+        
+        let imageName = String(numImage) + ".png"
+        imgView.image = UIImage(named: imageName)
+    }
 }
 
