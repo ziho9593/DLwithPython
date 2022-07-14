7688b99680f1a2c38e67093317cb09c0b57bd43e
ziho9593 <ziho9593@gmail.com>
2022-07-14 14:05:45 +0900

    데이트 피커 사용해 날짜 선택하기

---
+++ Books/Swift-Basic/04/DatePicker/DatePicker/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by Jiho on 2022/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+//        lblCurrentTime.text = String(count)
+//        count += 1
+        
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+    }
+}
+
