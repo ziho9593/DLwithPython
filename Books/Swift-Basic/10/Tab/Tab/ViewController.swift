2ce67148d958642b652510beb9bcc3787c140346
ziho9593 <ziho9593@gmail.com>
2022-07-24 20:07:49 +0900

    탭 바 컨트롤러 이용해 여러 개의 뷰 넣기

---
+++ Books/Swift-Basic/10/Tab/Tab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by Jiho on 2022/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+    
+}
+
