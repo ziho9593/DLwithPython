6c766b57f011ecc3b7d75ac8faab4a47fb14a9b7
ziho9593 <ziho9593@gmail.com>
2022-07-24 18:40:41 +0900

    페이지 이동하기 - 페이지 컨트롤

---
+++ Books/Swift-Basic/09/PageControl/PageControl/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by Jiho on 2022/07/24.
+//
+
+import UIKit
+
+var images = ["01.png", "02.png", "03.png", "04.png", "05.png", "06.png"]
+
+class ViewController: UIViewController {
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.green
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        imgView.image = UIImage(named: images[0])
+    }
+
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
+    }
+    
+}
+
