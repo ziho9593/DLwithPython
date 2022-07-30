1fa6b959b13fed27af10199ca9bb5866ce95e906
ziho9593 <ziho9593@gmail.com>
2022-07-30 16:45:23 +0900

    테이블 뷰 컨트롤러 이용해 할 일 목록 만들기

---
+++ Books/Swift-Basic/12/Table/Table/AddViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddViewController.swift
+//  Table
+//
+//  Created by Jiho on 2022/07/30.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    @IBOutlet var txtAddItem: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnAddItem(_ sender: UIButton) {
+        items.append(txtAddItem.text!)
+        itemsImageFile.append("clock.png")
+        txtAddItem.text = ""
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
